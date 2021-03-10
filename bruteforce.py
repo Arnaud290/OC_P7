4efd5b17aa2a4ff924bcb4e4d3c7c673d34693cb
Arnaud290 <arnaud.manach@gmail.com>
2021-03-10 20:16:09 +0100

    optimisation des scripts

--- bruteforce.py
+++ bruteforce.py
@@ -1,31 +1,40 @@
 """Program brute force"""
 import csv
 from time import time, strftime, gmtime
-
+import os
 
 def brutforce_buy_actions(action_list_csv):
+    CASH = 500
+    iteration = 0
     time_start = time()
     actions_list = []
     total_result = []
     with open(action_list_csv, "r") as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
         for row in reader:
-            benefit_result = (float(row['price']) * float(row['profit']))/100
-            actions_list.append([row['name'], float(row['price']), float(row['profit']), benefit_result])
+            max_nb_buy = int(CASH // float(row['price']))
+            benefit_result = (float(row['price']) * float(row['profit'])) / 100
+            max_benefit_result = round((benefit_result * max_nb_buy), 2)
+            actions_list.append([row['name'],
+                                float(row['price']),
+                                float(row['profit']),
+                                benefit_result,
+                                max_benefit_result
+                                ])
     k = len(actions_list) - 1
     action_list_2 = actions_list
-    action_list_2.sort(key=lambda list: list[3], reverse=True)
+    action_list_2.sort(key=lambda list: list[4], reverse=True)
     for i in range(k):
         for j in range(0, k - i - 1):
             for share in actions_list:
-                cash = 500
+                cash = CASH
                 benefit = 0
                 nb_actions = 0
                 while share[1] <= cash:
                     cash -= share[1]
                     nb_actions += 1
                     benefit += share[3] 
-                    result_1 = share[0] + ' X ' + str(nb_actions)  
+                    result_1 = (share[0], nb_actions, round((nb_actions * share[3]), 2))  
                     cash_rest = cash
                     benefit_2 = 0
                     t_result = [result_1]
@@ -36,9 +45,12 @@ def brutforce_buy_actions(action_list_csv):
                                 cash_rest -= share_2[1]
                                 nb_action2 += 1
                                 benefit_2 += share_2[3]
+                                iteration += 1
                             if nb_action2 > 0:
-                                t_result.append(share_2[0] + ' X ' + str(nb_action2))  
-                    t_result.append('REST:' + str(cash_rest) + ' €') 
+                                t_result.append((share_2[0],
+                                                nb_action2,
+                                                round((nb_action2 * share_2[3]), 2)))  
+                    t_result.append(cash_rest) 
                     t_result.append(round(benefit + benefit_2, 2))
                     if t_result not in total_result:
                         total_result.append(t_result)
@@ -47,17 +59,23 @@ def brutforce_buy_actions(action_list_csv):
     total_result.sort(key=lambda x: x[-1], reverse=True)
     time_total = time() - time_start
     time_total = strftime('%H:%M:%S', gmtime(time_total))
-    return([total_result, time_total])
+    return([total_result, time_total, iteration])
 
 if __name__ == "__main__":
-  
-    result = brutforce_buy_actions("dataset2.csv")
-
+    os.system('clear')
+    result = brutforce_buy_actions("20_actions.csv")
     print("Temps total d'execution: ", result[1])
     print("\n\n")
     print("Nombre de résultats: " + str(len(result[0])))
     print("\n\n")
-    print("Meilleur résultat: ", result[0][0])
+    print("Nombre d'itérations': " + str(result[2]))
     print("\n\n")
-    print("Pire résultat: ", result[0][-1])
-   
+    print("Meilleur résultat: \n\n")
+    TEXT = "l'action: {}, acheté {} fois, pour un bénéfice de {} € au bout de 2 ans\n"
+    for i in range(len(result[0][0]) - 2):
+        share = result[0][0][i]
+        print(TEXT.format(share[0], share[1], share[2]))  
+    print("\n\n")
+    print("il reste {} €".format(result[0][0][-2]))
+    print("\n\n")
+    print("Le montant total des bénéfices est de {} €".format(result[0][0][-1]))
--- optimized.py
+++ optimized.py
@@ -1,23 +1,29 @@
 """Program brute force"""
+import os
 import csv
 from time import time, strftime, gmtime
 
-def buy_actions_optimized(action_list_csv):
+def optimized_buy_actions(action_list_csv):
     time_start = time()
     actions_list = []
-
+    result_list_best = []
+    CASH = 500
+    iteration = 0
     with open(action_list_csv, "r") as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
         for row in reader:
             if float(row['price']) > 0:
-                benefit_result =  (float(row['price']) * float(row['profit']))/100
-                actions_list.append([row['name'], float(row['price']), float(row['profit']), benefit_result])      
-     
-    actions_list.sort(key=lambda list: list[2], reverse=True)
-
-    #Best list  
-    result_list_best = []
-    cash = 500
+                max_nb_buy = int(CASH // float(row['price']))
+                benefit_result = (float(row['price']) * float(row['profit'])) / 100
+                max_benefit_result = round((benefit_result * max_nb_buy), 2)
+                actions_list.append([row['name'],
+                                    float(row['price']),
+                                    float(row['profit']),
+                                    benefit_result,
+                                    max_benefit_result
+                                    ])     
+    actions_list.sort(key=lambda list: list[4], reverse=True)
+    cash = CASH
     benefit = 0
     for share in actions_list:
         nb_buy_share = 0
@@ -25,34 +31,30 @@ def buy_actions_optimized(action_list_csv):
             cash -= share[1]
             nb_buy_share += 1
             benefit += share[3]
+            iteration += 1
         if  nb_buy_share > 0:
-            result_list_best.append(share[0] + ' X ' + str(nb_buy_share))
-    result_list_best.append('REST: ' + str(round(cash, 2)))
-    result_list_best.append('BENEFIT: ' + str(round(benefit, 2)))    
-
-    actions_list.sort(key=lambda list: list[2])
-
-    #Worst List
-    result_list_worst = []
-    cash = 500
-    benefit = 0
-    for share in actions_list:
-        nb_buy_share = 0
-        while cash >= share[1]:
-            cash -= share[1]
-            nb_buy_share += 1
-            benefit += share[3]
-        if  nb_buy_share > 0:
-            result_list_worst.append(share[0] + ' X ' + str(nb_buy_share))
-    result_list_worst.append('REST: ' + str(round(cash, 2)) + ' €')
-    result_list_worst.append('BENEFIT: ' + str(round(benefit, 2)) + ' €')
+            result_list_best.append((share[0], nb_buy_share, round((share[3] * nb_buy_share), 2)))
+    result_list_best.append(round(cash, 2))
+    result_list_best.append(round(benefit, 2)) 
+    result_list_best.append(iteration)    
     time_total = time() - time_start
     time_total = strftime('%H:%M:%S', gmtime(time_total))
-    return([result_list_best, result_list_worst, time_total])
-
+    result_list_best.append(time_total)
+    return result_list_best
+    
 if __name__ == "__main__":
-
-    result = buy_actions_optimized("dataset2.csv")
-    print("Temps total d'execution: ", result[2], "seconde")
-    print("\n\n Meilleur résultat: ", result[0])
-    print("\n\n pire résultat: ", result[1])
+    os.system('clear')
+    result = optimized_buy_actions("dataset2.csv")
+    print("Temps total d'execution: ", result[-1])
+    print("\n\n")
+    print("Nombre d'itérations': " + str(result[-2]))
+    print("\n\n")
+    print("Meilleur résultat: \n\n")
+    TEXT = "l'action: {}, acheté {} fois, pour un bénéfice de {} € au bout de 2 ans\n"
+    for i in range(len(result) - 4):
+        share = result[i]
+        print(TEXT.format(share[0], share[1], share[2]))  
+    print("\n\n")
+    print("il reste {} €".format(result[-4]))
+    print("\n\n")
+    print("Le montant total des bénéfices est de {} €".format(result[-3]))
